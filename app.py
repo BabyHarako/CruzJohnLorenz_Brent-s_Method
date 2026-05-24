806d2644d8db540cfb3e9e727368d5ed0779475e
CruzJohnLorenz <73048005+BabyHarako@users.noreply.github.com>
2026-05-24 11:58:12 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 import math
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='.')
 
 
 def safe_eval(expr: str, x: float) -> float:
